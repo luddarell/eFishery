db7e61549eb6e8934a2f36e854b9e87b0a2e0b13
Ludgerus Darell <lud.darell@gmail.com>
2021-07-02 17:55:32 +0700

    Add files via upload
    Start Iteration for counting each fish based on quanitity

--- Soal2/FishCounter.py
+++ Soal2/FishCounter.py
@@ -34,3 +34,35 @@ def prepro_berat(berat):
   for i in range(0, len(res)):
     res[i] = int(res[i])
   return res
+
+  fish_count={}
+prepro_komoditas=prepo_komoditas(df_review['komoditas']
+for i in range(len(df_review[:4])):
+  for j in range(len(prepo_komoditas(df_review['komoditas'][i]))):
+    for k in range(len(prepro_komoditas(df_review['komoditas'][i]))):
+      if prepro_komoditas(df_review['komoditas'][i]) not in fish_count:
+        fish_count[i]=df_review['komoditas'][i]
+
+fish_count
+
+df_review['komoditas'][:4]
+
+prepro_komoditas(df_review['komoditas'][1])
+
+for i in range(len(df_review['komoditas'])):
+  df_review['komoditas'][i]=prepro_komoditas(df_review['komoditas'][i])
+
+df_review['komoditas']
+
+for i in range(len(df_review['berat'])):
+  df_review['berat'][i]=prepro_berat(df_review['berat'][i])
+
+test_df=df_review[:4]
+
+test_df
+
+fish_counter={}
+for i in range
+for j in test_df.komoditas:
+  for k in test_df.komoditas:
+    print(k)
