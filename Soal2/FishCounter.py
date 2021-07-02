3f639a97ee5f4a26ba77af2fceb4e011cf93f9a9
Ludgerus Darell <lud.darell@gmail.com>
2021-07-02 19:07:12 +0700

    Add files via upload
    Add some comment

--- Soal2/FishCounter.py
+++ Soal2/FishCounter.py
@@ -1,15 +1,18 @@
-import pandas as pd
-import numpy as np
-import nltk
+# Add some Library
+import pandas as pd # For Dataframe/JSON processing 
+import numpy as np # For Matrix operation
+import nltk # Language Processing
 from nltk.corpus import stopwords
-nltk.download('stopwords')
+nltk.download('stopwords') #Stopword
 from nltk.tokenize import word_tokenize
-import re
+import re # Regex
 from gensim.parsing.preprocessing import STOPWORDS
 nltk.download('punkt')
 
+#Load the dataset from JSON file
 df_review=pd.read_json('drive/MyDrive/soal-2.json')
 
+#Create method for preprocessing text in 'KOMODITAS', to remove any stopword except the fish name.
 def prepro_komoditas(komoditas):
   all_stopwords_gensim = STOPWORDS.union(set(['ikan']))
   text = komoditas
@@ -23,6 +26,7 @@ def prepro_komoditas(komoditas):
   else:
     return tokens_without_sw
 
+#Create method for preprocessing text and number in 'BERAT', to remove any stopword except the fish weight number.
 def prepro_berat(berat):
   all_stopwords_gensim = STOPWORDS.union(set(['ikan','lele','bawal','nila','pindang tongkol','gurame','mas','mujaer','kembung','salem','rata2','rata rata','rata','sampe','kurang','-','gak nentu','kadang','s.d']))
   text = berat
@@ -43,26 +47,12 @@ for i in range(len(df_review[:4])):
       if prepro_komoditas(df_review['komoditas'][i]) not in fish_count:
         fish_count[i]=df_review['komoditas'][i]
 
-fish_count
-
-df_review['komoditas'][:4]
-
-prepro_komoditas(df_review['komoditas'][1])
-
+#Initialize for Preprocessing step in KOMODITAS
 for i in range(len(df_review['komoditas'])):
   df_review['komoditas'][i]=prepro_komoditas(df_review['komoditas'][i])
 
 df_review['komoditas']
 
+#Initialize for Preprocessing step in BERAT
 for i in range(len(df_review['berat'])):
   df_review['berat'][i]=prepro_berat(df_review['berat'][i])
-
-test_df=df_review[:4]
-
-test_df
-
-fish_counter={}
-for i in range
-for j in test_df.komoditas:
-  for k in test_df.komoditas:
-    print(k)
